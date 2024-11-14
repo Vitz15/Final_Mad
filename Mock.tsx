e98fce1e66fc574ffd18678baec8c4ea6ddd08d4
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-14 10:50:51 +0800

    add search

--- Mock.tsx
+++ Mock.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Sickness from './src/pages/CheckSickness';
+import Search from './src/components/molecules/Search';
 
 const Mock = () => {
   return (
--- src/pages/CheckSickness/index.tsx
+++ src/pages/CheckSickness/index.tsx
@@ -1,6 +1,7 @@
-import {StyleSheet, Text, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
 import ListBar from '../../components/molecules/ListBar';
+import Search from '../../components/molecules/Search';
 import {
   Fever,
   Pneunomia,
@@ -12,34 +13,41 @@ import {
   Gerd,
   Diarhea,
   Sepsis,
+  SearchLogo,
 } from '../../assets/icon';
 import {Gap} from '../../components/atoms';
 
 const Sickness = () => {
   return (
-    <ScrollView style={styles.bar}>
-      <Gap height={33} />
-      <ListBar type="left" source={Fever} />
-      <Gap height={20} />
-      <ListBar type="right" source={Pneunomia} />
-      <Gap height={30} />
-      <ListBar type="left" source={Stroke} />
-      <Gap height={20} />
-      <ListBar type="right" source={Flu} />
-      <Gap height={30} />
-      <ListBar type="left" source={Asthma} />
-      <Gap height={20} />
-      <ListBar type="right" source={ChestPain} />
-      <Gap height={30} />
-      <ListBar type="left" source={Malaria} />
-      <Gap height={20} />
-      <ListBar type="right" source={Gerd} />
-      <Gap height={30} />
-      <ListBar type="left" source={Sepsis} />
-      <Gap height={30} />
-      <ListBar type="right" source={Diarhea} />
-      <Gap height={30} />
-    </ScrollView>
+    <View>
+      <Search
+       placeholder="Find Your Symptom"
+       icon={SearchLogo} 
+      />
+      <ScrollView style={styles.bar}>
+        <Gap height={33} />
+        <ListBar type="left" source={Fever} />
+        <Gap height={20} />
+        <ListBar type="right" source={Pneunomia} />
+        <Gap height={30} />
+        <ListBar type="left" source={Stroke} />
+        <Gap height={20} />
+        <ListBar type="right" source={Flu} />
+        <Gap height={30} />
+        <ListBar type="left" source={Asthma} />
+        <Gap height={20} />
+        <ListBar type="right" source={ChestPain} />
+        <Gap height={30} />
+        <ListBar type="left" source={Malaria} />
+        <Gap height={20} />
+        <ListBar type="right" source={Gerd} />
+        <Gap height={30} />
+        <ListBar type="left" source={Sepsis} />
+        <Gap height={30} />
+        <ListBar type="right" source={Diarhea} />
+        <Gap height={30} />
+      </ScrollView>
+    </View>
   );
 };
 
