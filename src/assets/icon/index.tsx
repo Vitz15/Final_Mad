d1f5c0da3896acc85b53af15efa1d52c2cc64da5
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-14 23:11:13 +0800

    add medicine carde molecule

--- src/assets/icon/index.tsx
+++ src/assets/icon/index.tsx
@@ -13,7 +13,8 @@ import Gerd from './Gerd.svg';
 import Sepsis from './Sepsis.svg';
 import Diarhea from './Diarhea.svg';
 import Picture from './Picture.svg';
-import SearchLogo from './Search.svg'
+import SearchLogo from './Search.svg';
+import Penicilin from './Rectangle 194.svg';
 
 export {
   Azzure,
@@ -31,5 +32,6 @@ export {
   Diarhea,
   Sepsis,
   Picture,
-  SearchLogo
+  SearchLogo,
+  Penicilin
 };
